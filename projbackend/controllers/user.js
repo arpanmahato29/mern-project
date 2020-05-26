32650b851f463dfc1684a9e31b750d1a335bda22
unknown <arpanmahato1920@gamil.com>
2020-05-26 15:47:33 +0530

    urser order list update

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const Users = require("../models/user");
+const Order = require("../models/order")
 
 
 
@@ -28,7 +29,7 @@ exports.updateUser = (req, res) => {
     Users.findByIdAndUpdate(
         {_id : req.profile._id},    //find the user
         {$set: req.body},           //update the selected field
-        {new: true, userFindAndModify: false},  //compulsory parameters
+        {new: true, useFindAndModify: false},  //compulsory parameters
         (err,user) =>{
             if(err) {
                 return req.status(400).json({
@@ -38,6 +39,50 @@ exports.updateUser = (req, res) => {
             user.salt = undefined
             user.encry_password = undefined
             res.json(user)
+        }
+    )
+}
+
+exports.userPurchaseList = (req,res) => {
+    Order.find({user : req.profile._id})
+    .populate("user","_id name")
+    .exec((err,order) => {
+        if(err){
+            return res.status(400).json({
+                error : "No order on this account"
+            })
+        }
+        return res.json(order);
+    })
+}
+
+exports.pushOrderInPurchaseList = (req,res,next) => {
+    let purchases = [];
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name : product.name,
+            description : product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount : req.boy.order.amount,
+            transaction_id : req.body.transaction_id
+        })
+    });
+
+    //store this in DB
+    Users.findOneAndUpdate(
+        {_id:req.profile._id},
+        {$push:{purchases:purchases}},
+        {new:true},
+        (err,purchases) => {
+            if(err) {
+                return res.status(400).json({
+                    error:"Unable to save perchase list"
+                })
+     
+           }
+           next()
         }
     )
 }

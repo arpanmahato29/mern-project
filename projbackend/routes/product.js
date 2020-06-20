cc1b429eba6a427b6e59b0de169b217c76fa438c
unknown <arpanmahato1920@gamil.com>
2020-06-20 18:19:43 +0530

    checker added

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const {check} = require("express-validator")
 
 //controllers
 const {getProductById,createProduct} = require("../controllers/product")
